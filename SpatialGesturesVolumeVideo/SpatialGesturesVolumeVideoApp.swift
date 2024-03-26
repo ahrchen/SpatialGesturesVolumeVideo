6b7afc893c3b0fb448cd353806e3a055b314e5af
Raymond Chen <ahrchen@gmail.com>
2024-03-26 13:40:32 -0700

    Initial Commit

---
+++ SpatialGesturesVolumeVideo/SpatialGesturesVolumeVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialGesturesVolumeVideoApp.swift
+//  SpatialGesturesVolumeVideo
+//
+//  Created by Raymond Chen on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SpatialGesturesVolumeVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
